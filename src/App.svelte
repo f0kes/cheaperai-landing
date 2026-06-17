e6cc532fd679e5de862f3e3e0564d8fc79d8bdd1
f0kes <f0kesppokes@gmail.com>
2026-06-18 01:44:49 +0300

    good result

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount, onDestroy } from 'svelte';
+  import Nav from './lib/Nav.svelte';
+  import Hero from './lib/Hero.svelte';
+  import Calculator from './lib/Calculator.svelte';
+  import LeadGen from './lib/LeadGen.svelte';
+  import Footer from './lib/Footer.svelte';
+</script>
+
+<Nav />
+<main>
+  <Hero />
+  <Calculator />
+  <LeadGen />
+  <Footer />
+</main>
